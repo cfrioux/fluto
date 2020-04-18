a0e21b448d5872ada0fa16af4e25d559f57047ec
Sven Thiele <sthiele78@gmail.com>
2020-04-18 02:35:23 +0200

    more tests

--- flutopy/tests/test.py
+++ flutopy/tests/test.py
@@ -10,6 +10,11 @@ def test_fluto():
     result = run_fluto(
         'data/toy/draft.xml', seeds=None, repairbase='data/toy/repairdb.xml', handorf=True, fluto1=False, no_fba=True, no_accumulation=False, cplex=False, json=True)
     assert result['Topological criterium'] == 'Handorf'
+    assert result['Flux balance criterium'] == 'OFF'
+    assert result['Objective reactions'] == ["R5"]
+    assert result['Producible targets'] == ["a", "c"]
+    assert result['Added reactions'] == ["R7", "R6"]
+    assert result['Accumulating metabolites'] == []
 
 
 test_fluto()
